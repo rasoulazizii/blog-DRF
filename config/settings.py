75003d536189512b16c4e706c9e664efbcfaf193
root <root@Rasoul.>
2025-11-05 13:27:48 +0330

    config: rest framwork basic config done

--- config/settings.py
+++ config/settings.py
@@ -37,11 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #rest framework
+    'rest_framework',
+    'rest_framework.authtoken',
 
     #apps
     'accounts',
     'blog',
 ]
+
+# rest authentication config
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

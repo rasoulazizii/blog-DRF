21dc312cb6b7b41e8be55d7d8150559059948736
root <root@Rasoul.>
2025-11-05 13:18:03 +0330

    blog: Post and Comment models create and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=220)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title[:50]}... by {self.author}'
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(540)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'comment: {self.body[:50]}... by {self.author} on {self.post}'
+
+    

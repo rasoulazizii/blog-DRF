e3e0eb784adef8ec6114621bac802b68f4668305
root <root@Rasoul.>
2025-11-05 16:20:43 +0330

    accounts: syntax erorr fix

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,11 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
-from .serializers import UserRegisterationModelSerializer
+from .serializers import UserRegistrationModelSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserRegisterationModelSerializer
+    serializer_class = UserRegistrationModelSerializer
     permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
